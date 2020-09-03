affe20ad49299ba52d79c8a6922854da807afa2f
liangjianchao <liangjc.get@gmail.com>
2020-09-04 00:02:29 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+module.exports = {
+  entry: {
+    main: "./main.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "createElement",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "main.html",
+      template: "public/main.html",
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
